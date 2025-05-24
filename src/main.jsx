884c66b5fd9f3cb2cae10fb7176f71eceae718c6
Pavel Hrynevich <qushoneo>
2025-05-24 22:49:40 +0300

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { MembersTableSkeleton } from "./components/profile_id/forum_skeleton/MemberTable.jsx";
 
 const root = createRoot(document.getElementById("root"));
@@ -22,11 +22,11 @@ const routes = [
 ];
 
 root.render(
-  <BrowserRouter basename="/">
+  <HashRouter>
     <Routes>
       {routes.map((route, idx) => (
         <Route key={idx} path={route.path} element={route.element} />
       ))}
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
